3581405eba276648aec54de823c1faccd70ad4ff
Gregory Loven <gregoryseminari@gmail.com>
2024-01-01 14:36:37 +0800

    side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,15 +5,10 @@
             </div>
             <ul class="sidebar-menu">
                 <li><a class="nav-link" href="/"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
-                <li><a class="nav-link" href="/category"><i class="far fa-file-alt"></i> <span>Category</span></a></li>
-                <li><a class="nav-link" href="/material"><i class="fas fa-map-marker-alt"></i> <span>Material</span></a></li>
-                <li><a class="nav-link" href="/product"><i class="fas fa-map-marker-alt"></i> <span>Product</span></a></li>
+                <li><a class="nav-link" href="/category"><i class="fas fa-list-alt"></i> <span>Category</span></a></li>
+                <li><a class="nav-link" href="/material"><i class="fas fa-pencil-alt"></i> <span>Material</span></a></li>
+                <li><a class="nav-link" href="/product"><i class="fas fa-hammer"></i> <span>Product</span></a></li>
             </ul>
-
-            <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-                <a href="https://wa.me/6282236639255" class="btn btn-primary btn-lg btn-block btn-icon-split">
-                <i class="fa fa-phone"></i> Hubungi Kami
-                </a>
-            </div>
+            
         </aside>
     </div>
